fc2e93a65edfe01125ed1ea6c420b1b91463bb91
groverjatin17 <groverjatin17@gmail.com>
2022-03-19 19:43:11 -0400

    Updates

---
+++ precache-manifest.c251af64334cfc736ac0881b46436584.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea958734f52efbe1a195a8dfc63f9244",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bef8da756a303ba10e9f",
+    "url": "/static/css/2.9fca9f18.chunk.css"
+  },
+  {
+    "revision": "54d376b2ecf8f762bfcd",
+    "url": "/static/css/main.c1d1cc42.chunk.css"
+  },
+  {
+    "revision": "bef8da756a303ba10e9f",
+    "url": "/static/js/2.d7fbe531.chunk.js"
+  },
+  {
+    "revision": "875f806f6d109f669eded812d85f1348",
+    "url": "/static/js/2.d7fbe531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d376b2ecf8f762bfcd",
+    "url": "/static/js/main.20cb6aa6.chunk.js"
+  },
+  {
+    "revision": "25aa74dc49e511cfc3ea",
+    "url": "/static/js/runtime-main.54d7c234.js"
+  },
+  {
+    "revision": "368ac849ed88ff7ac11f4b96bed2ae1e",
+    "url": "/static/media/athlete.368ac849.png"
+  },
+  {
+    "revision": "d25f025c58c2fb779bb2e29fdc9b06a8",
+    "url": "/static/media/call-icon-footer.d25f025c.svg"
+  },
+  {
+    "revision": "792dd0916ba8af097362329b61448ec5",
+    "url": "/static/media/call-icon.792dd091.svg"
+  },
+  {
+    "revision": "c5eda5ae17833cae3b62372d3f52748e",
+    "url": "/static/media/close.c5eda5ae.svg"
+  },
+  {
+    "revision": "84330d8467a887a24b041755829f86ce",
+    "url": "/static/media/delivery.84330d84.jpg"
+  },
+  {
+    "revision": "8cabc0e9de2b56ea6aea99b92a624429",
+    "url": "/static/media/disease01.8cabc0e9.jpg"
+  },
+  {
+    "revision": "2a897f93565a1bac9e832696cf8670f3",
+    "url": "/static/media/disease02.2a897f93.jpg"
+  },
+  {
+    "revision": "6bd8996c4f9d79ada087a0452cdba645",
+    "url": "/static/media/disease03.6bd8996c.jpg"
+  },
+  {
+    "revision": "a479dfbc621d797da32048bad7401e3b",
+    "url": "/static/media/disease04.a479dfbc.jpg"
+  },
+  {
+    "revision": "b3b240780b39dd79d746c615df13e273",
+    "url": "/static/media/doctor2.b3b24078.jpg"
+  },
+  {
+    "revision": "51429c0a6190793fc2addb736ea05135",
+    "url": "/static/media/gallery1.51429c0a.jpg"
+  },
+  {
+    "revision": "bcba9d0684eaa36591eae1af5fb45e2d",
+    "url": "/static/media/gallery2.bcba9d06.jpg"
+  },
+  {
+    "revision": "5668eb68dfe1db92cf7544862606b5a9",
+    "url": "/static/media/gallery3.5668eb68.jpg"
+  },
+  {
+    "revision": "15919d48a17f1124275fae8b2aa98b90",
+    "url": "/static/media/gallery4.15919d48.jpg"
+  },
+  {
+    "revision": "85b33b1fdde3f8f73ec43c0bc99f86e5",
+    "url": "/static/media/gallery5.85b33b1f.jpg"
+  },
+  {
+    "revision": "57f5104ccfb8e8ddb95d37b3f40303b3",
+    "url": "/static/media/gallery6.57f5104c.jpg"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "22bc2e5a6c84b80ce35b2dd8aeb5e9dc",
+    "url": "/static/media/hospital-logo.22bc2e5a.svg"
+  },
+  {
+    "revision": "57bf48421e21a4b2631a413ac4607fbc",
+    "url": "/static/media/hospital-name-mobile.57bf4842.svg"
+  },
+  {
+    "revision": "5058093ea2e932d375041c0fe5ae7bc8",
+    "url": "/static/media/hospital-name.5058093e.svg"
+  },
+  {
+    "revision": "4de8c429fe4e549b62d6f31a693cc664",
+    "url": "/static/media/ishan.4de8c429.jpg"
+  },
+  {
+    "revision": "84a2e4d9373bdadec9b3ab993cb4bc4e",
+    "url": "/static/media/laboratory.84a2e4d9.jpg"
+  },
+  {
+    "revision": "fa08549c7ebeee43fc8cf8e11cfdead4",
+    "url": "/static/media/map-marker.fa08549c.svg"
+  },
+  {
+    "revision": "276c159659dc138b635d7c1d511d06c9",
+    "url": "/static/media/menu-icon.276c1596.svg"
+  },
+  {
+    "revision": "1b3683e924fa93cc85c11af3074925d5",
+    "url": "/static/media/muscular-system.1b3683e9.jpg"
+  },
+  {
+    "revision": "0bb0838d23a4f03e6b2b98961b2b3625",
+    "url": "/static/media/physio.0bb0838d.jpg"
+  },
+  {
+    "revision": "962eba8795a060dce4ce3c0c79bbced4",
+    "url": "/static/media/surgery.962eba87.jpg"
+  },
+  {
+    "revision": "a8a7981c6bb9b2fd0d8f844bd442811f",
+    "url": "/static/media/testimonial1.a8a7981c.jpg"
+  },
+  {
+    "revision": "469a73d21e32f2f5ca7ed41fee80c081",
+    "url": "/static/media/testimonial2.469a73d2.jpg"
+  },
+  {
+    "revision": "716e46bf6797c937f9ea472728ae5f67",
+    "url": "/static/media/ultrasound.716e46bf.jpg"
+  },
+  {
+    "revision": "9d7928f9bf3632a81d087a3fc9b52921",
+    "url": "/static/media/xray.9d7928f9.jpg"
+  }
+]);
